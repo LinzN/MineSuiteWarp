e0572b5a8bf9c2b3540b048ceed4d16b23446339
Niklas Note <niklaslinz@hotmail.com>
2016-06-06 20:47:58 +0200

    Formate and add own repo

--- src/main/java/de/nlinz/xeonSuite/warp/commands/WarpCommand.java
+++ src/main/java/de/nlinz/xeonSuite/warp/commands/WarpCommand.java
@@ -44,30 +44,30 @@ public class WarpCommand implements CommandExecutor {
 											String.valueOf(XeonSuiteBukkit.getWarmUpTime())));
 									Warpplugin.inst().getServer().getScheduler().runTaskLater(Warpplugin.inst(),
 											new Runnable() {
-										@Override
-										public void run() {
+												@Override
+												public void run() {
 
-											Location loc = WarpDataTable.lastWarpLocation.get(player);
-											WarpDataTable.lastWarpLocation.remove(player);
-											if ((loc != null)
-													&& (loc.getBlock().equals(player.getLocation().getBlock()))) {
-												List<String> list = WarpSqlActions.getWarp(warpName);
-												String world = list.get(1);
-												String server = list.get(2);
-												double x = Double.parseDouble(list.get(3));
-												double y = Double.parseDouble(list.get(4));
-												double z = Double.parseDouble(list.get(5));
-												float yaw = Float.parseFloat(list.get(6));
-												float pitch = Float.parseFloat(list.get(7));
-												WPStreamOutApi.sendTeleportToWarpOut(player.getName(), server, world, x,
-														y, z, yaw, pitch);
+													Location loc = WarpDataTable.lastWarpLocation.get(player);
+													WarpDataTable.lastWarpLocation.remove(player);
+													if ((loc != null) && (loc.getBlock()
+															.equals(player.getLocation().getBlock()))) {
+														List<String> list = WarpSqlActions.getWarp(warpName);
+														String world = list.get(1);
+														String server = list.get(2);
+														double x = Double.parseDouble(list.get(3));
+														double y = Double.parseDouble(list.get(4));
+														double z = Double.parseDouble(list.get(5));
+														float yaw = Float.parseFloat(list.get(6));
+														float pitch = Float.parseFloat(list.get(7));
+														WPStreamOutApi.sendTeleportToWarpOut(player.getName(), server,
+																world, x, y, z, yaw, pitch);
 
-												return;
-											} else {
-												player.sendMessage(GlobalMessageDB.TELEPORT_MOVE_CANCEL);
-											}
-										}
-									}, 20L * XeonSuiteBukkit.getWarmUpTime());
+														return;
+													} else {
+														player.sendMessage(GlobalMessageDB.TELEPORT_MOVE_CANCEL);
+													}
+												}
+											}, 20L * XeonSuiteBukkit.getWarmUpTime());
 								} else {
 									List<String> list = WarpSqlActions.getWarp(warpName);
 									String world = list.get(1);
